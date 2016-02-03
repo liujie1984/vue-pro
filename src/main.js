aa27ec6b5ddb0c73f67a8bc30ab6df3e059dcaff
facejiong <2279763407@qq.com>
2016-02-03 17:26:09 +0800

    router

--- src/main.js
+++ src/main.js
@@ -1,21 +1,78 @@
-var App = Vue.extend({});
-var router = new VueRouter();
-var Foo = Vue.extend({
-    template: '<p>This is foo!</p>'
-})
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
-var Bar = Vue.extend({
-    template: '<p>This is bar!</p>'
-})
-router.map({
-    '/foo': {
-        component: Foo
-    },
-    '/bar': {
-        component: Bar
-    }
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+var App = Vue.extend({});
+var router = new VueRouter({
+    hashbang: true,
+    history: false,
+    saveScrollPosition: true,
+    transitionOnLoad: true
 });
 
-// start app
+router.map({
+    '/':{				//首页-推荐
+        name:'recommend',
+        component:require('./views/recommend.vue')
+    },
+    '/recommend':{				//首页-推荐
+        name:'recommend',
+        component:require('./views/recommend.vue')
+    },
+    /* 404路由 */
+    '*': {
+        component: require('./views/recommend.vue')
+    },
+    '/image':{               //图片
+        name:'image',
+        component:require('./views/image.vue')
+    },
+    '/video':{               //视频
+        name:'video',
+        component:require('./views/video.vue')
+    },
+    '/focus':{               //视频
+        name:'focus',
+        component:require('./views/focus.vue')
+    },
+    '/tutorial':{               //视频
+        name:'tutorial',
+        component:require('./views/tutorial.vue')
+    },
+    '/review':{               //视频
+        name:'review',
+        component:require('./views/review.vue')
+    }
 
+    //'/topic/:id':{               //专题
+    //    name:'topic',
+    //    component:require('./views/topic.vue')
+    //},
+    //'/add':{               //首页
+    //    name:'add',
+    //    component:require('./views/new.vue'),
+    //    auth: true
+    //},
+    //'/message':{               //消息
+    //    name:'message',
+    //    component:require('./views/message.vue'),
+    //    auth: true
+    //}
+    //'/about':{               //关于
+    //    name:'about',
+    //    component:require('./views/about.vue')
+    //},
+    //'/login':{               //登录
+    //    name:'login',
+    //    component:require('./views/login.vue')
+    //},
+    //'/user/:loginname':{               //用户信息
+    //    name:'user',
+    //    component:require('./views/user.vue')
+    //}
+});
+// start app
 router.start(App, '#app');
---
+++ src/views/recommend.vue
@@ -0,0 +1,15 @@
+<template>
+    <!-- 全局header -->
+    <nav-top type="recommend"></nav-top>
+</template>
+<script>
+    export default {
+        components:{
+            'nav-top':require('../components/nav-top.vue')
+        }
+    }
+</script>
+<style lang="less">
+
+</style>
+
