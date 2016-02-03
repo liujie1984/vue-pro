86c6bb7fabfd06c64af8c70e8a88094d98e2fc5a
facejiong <2279763407@qq.com>
2016-02-03 21:09:54 +0800

    props

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -1,21 +1,25 @@
 <style lang="less">
-
+    #banner{width:100%;height:140px;overflow:hidden;}
+    #banner-image{height:140px;width:100%;}
+    #banner-choose{margin-top:-25px;height:10px;text-align:center;}
+    #banner-choose li{display: inline-block;width: 5px;height: 5px;border-radius: 5px;
+        background-color: white;margin:0 3px 0 3px;}
 </style>
 <template>
-<ul>
-    <li>banner</li>
-</ul>
+<div id="banner">
+    <img id="banner-image" src="{{bannerList[0].image}}">
+    <ul id="banner-choose">
+        <li v-for="bannerList in bannerList" id="{{bannerList.item_id}}"></li>
+    </ul>   
+</div>
 </template>
 <script>
     export default {
-        props: {
-            bannerList:Object,
-        },
-        ready: function(){
-            console.dir(this.bannerList);
-        },
-        attached:function(){
-            console.dir(this.bannerList);
+        props: ['bannerList'],
+        methods:{
+            bannerChange:function(){
+
+            }
         }
     };
 </script>
--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,18 +1,20 @@
 <template>
     <!-- 全局header -->
     <nav-top type="recommend"></nav-top>
-    <banner :banner-list="bannerList"></banner>	
+    <banner v-bind:banner-list="bannerList"></banner>	
 </template>
 <script>
     export default {
-    	data:{
-    		bannerList:'1',
-    		worksList:'1'
+    	data:function(){
+    		return {
+    			bannerList:'',
+    			worksList:''
+    		}
     	},
     	ready:function() {
       		this.$http({url: 'mock/recommend.json', method: 'GET'}).then(function (response) {
       			this.bannerList = response.data.data.banner_list;
-      			this.worksList = JSON.stringify(response.data.data.list);
+      			this.worksList = response.data.data.list;
       			console.dir(this.bannerList);
       			console.dir(response);
       		}, function (response) {
